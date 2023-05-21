501a0e9da9140db73735f329a9884fd4a783cc36
Deepjyoti Baishya <deepjyoti120281@gmail.com>
2023-05-21 10:44:25 +0530

    done

--- lib/views/wallet/payment_information.dart
+++ lib/views/wallet/payment_information.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:rashi_network/ui/theme/text.dart';
 import 'package:rashi_network/ui/widgets/material_iconbtn.dart';
 import 'package:rashi_network/utils/design_colors.dart';
